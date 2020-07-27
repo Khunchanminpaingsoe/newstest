33a9c21b2905db74cd14a5a19be7c977c7f63576
Khunchanminpaingsoe <khunchanminpaingsoe1500@gmail.com>
2020-07-27 12:26:09 +0630

    fiel upload

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const route = require('./routes/news_route');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const { render } = require('ejs');
 const app = express();
-
-app.use(bodyParser.urlencoded({extended: true}));
+const cors = require('cors')
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin',"*");
+  res.header('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Authorization');
+  if(req.method === "OPTION"){
+    res.header('Access-Control-Allow-Methods','GET,POST,DELETE,PUT,PATCH');
+    return res.status(200).json({});
+  }
+  next();
+})
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname));
+app.use(expressLayouts);
+
+app.use(methodOverride('_method'));
 
 const db = require('./config/keys').MongoURI;
 
@@ -17,11 +36,13 @@ mongoose
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-  const PORT = process.env.PORT || 8000;
+  const PORT = process.env.PORT || 8500;
 
-app.use('/image', express.static('./uploads'));
+  
+
 app.use('/',route);
+//app.use('/layout',require('./routes/layout'));
 
 app.listen(PORT, () => {
-    console.log('Server is running at port 8000');
+    console.log('Server is running at port 8500');
 })
--- routes/news_route.js
+++ routes/news_route.js
@@ -1,30 +1,61 @@
 const express = require('express');
-const multer = require('multer');
+//const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const formidable = require('formidable');
+const detect = require('detect-file-type');
+const {v1: uuidv1} = require('uuid');
 const router = express.Router();
+const mv = require('mv');
 
-
-//const mongoose = require('mongoose');
-
-const storage = multer.diskStorage({
+/*const storage = multer.diskStorage({
     destination : (req,file,cb)=>{
-        cb (null, 'uploads/');
+        cb (null, './uploads/');
     },
     filename : (req,file,cb)=>{
-        cb(null, file.fieldname + '_' + Date.now() + file.originalname);
-      //cb(null, new Date().toISOString() + file.originalname);
+        cb(null, file.fieldname + '_' + Date.now() +  path.extname(file.originalname));
     }
   });
   
-const upload = multer({storage: storage});
+const upload = multer({storage: storage, limits: 1000000, fileFilter: (req, file, cb)=>{ checkFileType(file, cb);}});
+function checkFileType(file, cb){
+
+    const filetypes = /jpg|jpeg|png|gif/;
+
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+    const mimetype = filetypes.test(file.mimetype);
+
+    if(mimetype && extname){
+        return cb(null, true);
+    }else{
+        cb('Error: Image Only');
+    }
+}*/
 const newsModel = require('../models/news_model');
 
-router.get('/news', (req, res) => {
+router.get('/', (req, res) => { 
+    res.render('create');
+});
+
+router.get('/createget', (req, res) => {
+    
+    /*const data = {
+        catname: req.params.catname,
+        title: req.params.title,
+        image: "uploads/" + req.params.image,
+        content: req.params.content
+
+    }*/
     newsModel.find({})
     .then((data) => {
-        res.send(data);
+       res.render('view_all', {
+            datas: data
+                    //});
     })
     .catch(err => console.log(err));
     });
+});
 
 router.get('/news/:catname', (req, res) => {
     newsModel.findOne({ catname: req.params.catname })
@@ -34,34 +65,47 @@ router.get('/news/:catname', (req, res) => {
     .catch(err => console.log(err));
     });
 
-router.post('/news', upload.single('image'), (req, res) => {
-    //console.log(req.file);
-    const news = new newsModel({
-    catname: req.body.catname,
-    title: req.body.title,
-    image: req.file.path,
-    content: req.body.content 
-    });
-    news.save()
-    .then((data) => {
-        console.log(data);
-        res.send(data);
+    //router.post('/createpost', upload.single('image'), (req, res) =>
+router.post('/createpost',(req,res)=>{
+    let form = new formidable.IncomingForm();
+    form.parse(req,(err,fields,files)=>{
+        var oldpath = files.image.path;
+        console.log(oldpath);
+       
+        var newpath = 'uploads/'+new Date().toISOString()+files.image.name;
+       mv(oldpath, newpath, function(err) {
+            if(err){
+                console.log(err)
+            }
+            else{
+                console.log('newpath')
+            }
+          });
     })
-    .catch( err => console.log(err));
+
+})
+router.get('/update/:id', (req, res) => {
+    newsModel.findById({_id: req.params.id})
+    .then((data) => {
+        res.render('update', {
+            data: data
+        });
+    }); 
 });
 
-router.put('/news/:id', (req, res) => {
+router.put('/update/:id', (req, res) => {
     newsModel.findByIdAndUpdate({_id: req.params.id}, req.body)
     .then((data) => {
         res.send(data);
+        //res.redirect('/createget');
     })
     .catch(err => console.log(err));  
 });
 
-router.delete('/news/:id', (req, res) => {
+router.get('/delete/:id', (req, res) => {
     newsModel.findByIdAndRemove({_id: req.params.id})
-    .then((data) => {
-        res.send(data);
+    .then(() => {
+        res.redirect('back');
     })
     .catch(err => console.log(err));  
 });
